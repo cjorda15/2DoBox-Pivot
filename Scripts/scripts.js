575f455a7e05353a3b2c4d223f1f3d130fc30443
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-02-15 15:40:39 -0700

    weirdness with Git, but made clear button work.

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -13,6 +13,7 @@ $("#submit").on('click', function(e){
 	data.unshift(card);
 	storeIdea();
 	printIdea();
+	clearInput();
 	// console.log(card.id);
 })
 
@@ -41,9 +42,7 @@ data.forEach(function(object) {
 			<div id="${object.id}" class="new-idea">
 				<header>
 					<h1 class="entry-title">${object.title}</h1>
-					<ul>
-						<li class="clear"></li>
-					</ul>
+					<button class="clear"></button>
 				</header>
 				<article>
 					<p>${object.body}</p>
@@ -61,11 +60,11 @@ $("#card-section").on('click','.upvote', function() {
 		if($(this).siblings(".quality").text() === "swill") {
 			$(this).siblings(".quality").text("plausible");
 			qualityVar = "plausible";
-			editIdea(this, qualityVar);
+			editQuality(this, qualityVar);
 		} else if ($(this).siblings(".quality").text() === "plausible") {
 			$(this).siblings(".quality").text("genius");
 			qualityVar = "genius"
-			editIdea(this, qualityVar);
+			editQuality(this, qualityVar);
 		} else if ($(this).siblings(".quality").text() === "genius"){
 			qualityVar = "genius";
 		}
@@ -76,39 +75,69 @@ $("#card-section").on('click','.downvote', function() {
 		if($(this).siblings(".quality").text() === "genius") {
 			$(this).siblings(".quality").text("plausible");
 			qualityVar = "plausible";
-			editIdea(this, qualityVar);
+			editQuality(this, qualityVar);
 		} else if ($(this).siblings(".quality").text() === "plausible") {
 			$(this).siblings(".quality").text("swill");
 			qualityVar = "swill";
-			editIdea(this, qualityVar);
+			editQuality(this, qualityVar);
 		} else if ($(this).siblings(".quality").text() === "plausible") {
 			qualityVar = "swill";
 		}
 });
-function editIdea(location, qualityVar){
+
+
+
+function editQuality(location, qualityVar){
 	//grab the objectId of the item you are editing
 	var objectId = $(location).parent().parent().attr("id");
 	//grab the cardData from the array.
 	data = JSON.parse(localStorage.getItem("Data Item"));
-	console.log("This is the String you parsed: ");
-	console.log(data);
 	//run a for loop to evaluate each object in the array.
 	//use if statement to determine if the the object.id and the id of div matches.
 	//if it does, return object.quality to the array.
 	data.forEach(function(object){
 		if (object.id == objectId){
 			object.quality = qualityVar;
-			console.log(object.quality);
-			console.log(object);
 			return object.quality;
 		}
 	});
-	console.log("Hooray?");
-	console.log(data);
 	//stringify new array.
 	//upload array to localStorage.
 	 stringData= JSON.stringify(data);
 	localStorage.setItem("Data Item", stringData);
+}
+
+$("#card-section").on('click','.clear', function() {
+	var idOfRemoved = $(this).parent().parent().attr("id")
+	console.log(idOfRemoved);
+	clear(this, idOfRemoved);
+	$(this).closest('.new-idea').remove();
+
+});
+
+function clear(location, idOfRemoved){
+	var objectId = $(location).parent().parent().attr("id");
+	var removedId = idOfRemoved;
+	data = JSON.parse(localStorage.getItem("Data Item"));
+	data = data.filter(function(object){
+			return object.id % objectId;
+		});
+	 stringData= JSON.stringify(data);
+	localStorage.setItem("Data Item", stringData);
+}
+
+$('aside').on('keyup', '#search', function(){
+	searchInput = $(this).val().toLowerCase();
+	console.log(searchInput);
+	titleBoxes = $('.entry-title').text().toLowerCase();
+	bodyBoxes = $('article p').text().toLowerCase();
+	console.log(titleBoxes + bodyBoxes);
+});
+
+
+function clearInput() {
+	$('#title-input').val('');
+	$('#content-input').val('');
 }
 
 //NEXT STEPS
