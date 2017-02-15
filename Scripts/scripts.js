9705fcf9656254bd00cb779a31a5e4df1f304acc
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-02-14 22:22:16 -0700

    HOLY SHIT YEAH I DID A THING AND IT WORKS.

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -35,7 +35,7 @@ function getIdea(){
 }
 
 function printIdea(){
-$("#card-section").empty();
+$("#card-section").html('');
 data.forEach(function(object) {
 	 	$("#card-section").append(`
 			<div id="${object.id}" class="new-idea">
@@ -61,25 +61,41 @@ $("#card-section").on('click','.upvote', function() {
 		if($(this).siblings(".quality").text() === "swill") {
 			$(this).siblings(".quality").text("plausible");
 			qualityVar = "plausible";
+			editIdea(this, qualityVar);
 		} else if ($(this).siblings(".quality").text() === "plausible") {
 			$(this).siblings(".quality").text("genius");
 			qualityVar = "genius"
+			editIdea(this, qualityVar);
+		} else if ($(this).siblings(".quality").text() === "genius"){
+			qualityVar = "genius";
 		}
-		editIdea(this, qualityVar);
 });
 
-
+$("#card-section").on('click','.downvote', function() {
+		var qualityVar = $(this).siblings(".quality").text();
+		if($(this).siblings(".quality").text() === "genius") {
+			$(this).siblings(".quality").text("plausible");
+			qualityVar = "plausible";
+			editIdea(this, qualityVar);
+		} else if ($(this).siblings(".quality").text() === "plausible") {
+			$(this).siblings(".quality").text("swill");
+			qualityVar = "swill";
+			editIdea(this, qualityVar);
+		} else if ($(this).siblings(".quality").text() === "plausible") {
+			qualityVar = "swill";
+		}
+});
 function editIdea(location, qualityVar){
 	//grab the objectId of the item you are editing
 	var objectId = $(location).parent().parent().attr("id");
 	//grab the cardData from the array.
-	var cardData = JSON.parse(localStorage.getItem("Data Item"));
+	data = JSON.parse(localStorage.getItem("Data Item"));
 	console.log("This is the String you parsed: ");
-	console.log(cardData);
+	console.log(data);
 	//run a for loop to evaluate each object in the array.
 	//use if statement to determine if the the object.id and the id of div matches.
 	//if it does, return object.quality to the array.
-	cardData.forEach(function(object){
+	data.forEach(function(object){
 		if (object.id == objectId){
 			object.quality = qualityVar;
 			console.log(object.quality);
@@ -88,21 +104,17 @@ function editIdea(location, qualityVar){
 		}
 	});
 	console.log("Hooray?");
-	console.log(cardData);
-
+	console.log(data);
 	//stringify new array.
 	//upload array to localStorage.
-	var ammendedData = JSON.stringify(cardData);
-	localStorage.setItem("Data Item", ammendedData);
+	 stringData= JSON.stringify(data);
+	localStorage.setItem("Data Item", stringData);
 }
 
 
 
-		// accessCard.filter(function(object) {
-		// 		return object.id % id != 0 {
-		// 		});
-		// console.log(accessCard);
-		// console.log(id);
+
+
 
 	// parse through stored data for object/key;value
 	//
