a6a03eac531785b14d34cd5d0da5fe5acd9a82eb
Chris Jordan <cjorda15@msudenver.edu>
2017-02-18 22:28:25 -0700

    added test file

---
+++ test/test.js
@@ -0,0 +1,39 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver_fx = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+var driver_chr = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+// var driver_saf = new webdriver.Builder()
+//     .forBrowser('safari')
+//  .build();
+
+searchTest(driver_fx);
+searchTest(driver_chr);
+ // searchTest(driver_saf);
+
+function searchTest(driver) {
+  driver.get('https://cjorda15.github.io/2DoBox-Pivot/');
+  driver.findElement(By.id('title-input')).sendKeys("woof");
+  driver.findElement(By.id('content-input')).sendKeys("Now!");
+  driver.findElement(By.id("submit")).click();
+
+  driver.sleep(2000).then(function() {
+    driver.findElement(By.className('entry-body')).getText().then(function(title) {
+        // driver.findElement(By.className('title-body')).getText().then(function(text){
+      if(title === 'Now!') {
+        console.log('Test passed');
+      } else {
+        console.log('Test failed');
+      }
+    });
+  });
+// });
+  //  driver.quit();
+}
