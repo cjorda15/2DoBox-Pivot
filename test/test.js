0f4e95bb262384813d3909515f09bd7957b4d965
Chris Jordan <cjorda15@msudenver.edu>
2017-02-19 12:59:08 -0700

    added test 5 & 6

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ var webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
 
-// var driver_fx = new webdriver.Builder()
-//     .forBrowser('firefox')
-//     .build();
+var driver_fx = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
 
 var driver_chr = new webdriver.Builder()
     .forBrowser('chrome')
@@ -14,24 +14,36 @@ var driver_chr = new webdriver.Builder()
 //     .forBrowser('safari')
 //  .build();
 
-// searchTest(driver_fx);
-searchTest(driver_chr);
- // searchTest(driver_saf);
+ logTest(driver_fx);
+ logTest(driver_chr);
+  // logTest(driver_saf);
 
 refreshTest(driver_chr)
+refreshTest(driver_fx)
+// refreshTest(driver_saf)
 
+
+// deleteTest(driver_saf)
+deleteTest(driver_fx)
 deleteTest(driver_chr)
 
+// checkUpVote(driver_saf)
+checkUpVote(driver_fx)
 checkUpVote(driver_chr)
 
+// checkDownVote(driver_saf)
+checkDownVote(driver_fx)
 checkDownVote(driver_chr)
 
+// checkSearch(driver_saf)
+checkSearch(driver_fx)
 checkSearch(driver_chr)
 
-function searchTest(driver) {
+function logTest(driver) {
   driver.get('https://cjorda15.github.io/2DoBox-Pivot/');
   driver.findElement(By.id('title-input')).sendKeys("Code");
   driver.findElement(By.id('content-input')).sendKeys("Now!");
+  driver.sleep(2000)
   driver.findElement(By.id("submit")).click();
   driver.sleep(2000).then(function() {
   driver.findElement(By.className('entry-body')).getText().then(function(text) {
@@ -55,6 +67,7 @@ function refreshTest(driver) {
       } else {
         console.log('Test 2 failed');
       }
+      driver.sleep(1000)
     });
   });
  };
@@ -76,6 +89,7 @@ function deleteTest(driver){
       } else {
         console.log('Test 3 failed');
       }
+      driver.sleep(1000)
     });
   });
  });
@@ -89,6 +103,7 @@ driver.findElement(By.className('quality')).getText().then(function(text){
   }else{
     console.log('Test 4 failed')
   }
+  driver.sleep(1000)
 })
 }
 
@@ -100,6 +115,7 @@ function checkDownVote(driver){
     }else{
       console.log('Test 5 failed')
     }
+    driver.sleep(1000)
   })
 }
 
@@ -114,15 +130,9 @@ function checkSearch(driver){
   }else{
     console.log("Test 6 failed")
   }
-  })
-}
+  driver.sleep(3000)
 
-
-function testId(driver){
-  driver.findElement(By.id('title-input')).sendKeys("Grateful");
-  driver.findElement(By.id('content-input')).sendKeys("Always");
-  driver.findElement(By.id('submit')).click().then(function(id){
-    
+  driver.quit();
 
   })
 }
